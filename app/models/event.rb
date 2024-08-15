ef0ff4a6a3ded4898631339d0a1522e8044b7ec2
Ivan Kocienski <ivan@gfsc.studio>
2024-08-15 22:27:04 +0100

    Event model, queries and simple tests

---
+++ app/models/event.rb
@@ -0,0 +1,67 @@
+class Event
+
+  attr_reader :id
+  attr_reader :name
+  attr_reader :summary
+  attr_reader :description
+  attr_reader :start_date
+  attr_reader :end_date
+  attr_reader :publisher_url
+  attr_reader :address
+  attr_reader :organizer_id
+
+  class EventAddress
+    attr_reader :street_address
+    attr_reader :post_code
+
+    def initialize(from)
+      return unless from.is_a?(Hash)
+      @street_address = from['streetAddress']
+      @post_code = from['postalCode']
+    end
+  end
+
+  def initialize(data)
+    return unless data.is_a?(Hash)
+    @id = data['id'].to_i
+    @name = data['name']
+    @summary = data['summary']
+    @description = data['description']
+    @start_date = Time.zone.parse(data['startDate'])
+    @end_date = Time.zone.parse(data['endDate'])
+    @publisher_url = data['publisherUrl']
+    @organizer_id = data['organizer']['id']
+
+    @address = EventAddress.new(data['address'])
+  end
+
+  def self.load_from_fixture(path)
+    Rails.logger.info "Loading event data from #{path}"
+    data = JSON.parse(File.open(path).read)
+    @events = data['data']['eventsByFilter'].map { |event_data| Event.new(event_data) }
+  end
+
+  def self.count
+    (@events || []).count
+  end
+
+  def self.find_in_future(date)
+    date = date.beginning_of_day
+
+    (@events || []).filter { |event| event.start_date.beginning_of_day >= date }
+  end
+
+  def self.find_on_day(date)
+    date = date.beginning_of_day
+
+    (@events || []).filter { |event| event.start_date.beginning_of_day === date }
+  end
+
+  def self.find_by_id(want_id)
+    return unless want_id.present?
+    want_id = want_id.to_i
+    (@events || []).find { |event| event.id == want_id }
+  end
+end
+
+
---
+++ spec/models/event_spec.rb
@@ -0,0 +1,46 @@
+
+require 'rails_helper'
+
+describe 'Event' do
+  context 'class' do
+    before :each do
+      Event.load_from_fixture Rails.root.join('db/fixtures/events.json')
+    end
+
+    context '::count' do
+      it 'returns count of all partners' do
+        expect(Event.count).to eq 2900
+      end
+    end
+
+    context '::find_in_future' do
+      # FIXME: use test fixtures and explain magic numbers better
+      it 'returns events from a given time' do
+        epoch = Time.new(2024, 6, 1)
+        found = Event.find_in_future(epoch)
+        expect(found.length).to be 1214
+      end
+    end
+
+    context '::find_on_day' do
+      # FIXME: use test fixtures and explain magic numbers better
+      it 'returns events on given day' do
+        epoch = Time.new(2024, 3, 10)
+        found = Event.find_on_day(epoch)
+        expect(found.length).to be 5
+      end
+    end
+
+    context '::find_by_id' do
+      it 'returns a Event if found' do
+        found = Event.find_by_id(331179)
+        expect(found).to be_a(Event)
+      end
+
+      it 'returns nil for missing ID' do
+        found = Event.find_by_id(123456)
+        expect(found).to be nil
+      end
+    end
+  end
+end

0b78d2cc0c97bd7e8b7fbb81a3e80abef8bbe2e2
Ivan Kocienski <ivan.kocienski@gmail.com>
2024-09-23 11:03:12 +0100

    Very crude first pass at home page
    Just a list of "Hot" events and partners.
    
    Also some random keywords

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,21 @@
 class HomeController < ApplicationController
+
   def root
+    hot_partners = Partner.order('random()').limit(10)
+
+    hot_events = EventInstance
+      .from_day_onward(Time.now)
+      .order('random()')
+      .limit(10)
+
+    @hot_feed = (hot_events + hot_partners).shuffle
+
+    @hot_keywords = Keyword
+      .joins(:partner_keywords)
+      .group(:partner_id)
+      .having("count(partner_id) > 0")
+      .order('random()')
+      .limit(10)
   end
 
   def about
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,60 @@
 module HomeHelper
+
+  def _render_partner_thing(partner)
+    link = link_to(partner.name, partner_path(partner))
+
+    "<div class='partner'>" +
+      "<h2>#{link}</h2>" +
+      "<p>#{partner.summary}</p>" +
+      "</div>"
+  end
+
+  def _render_keyword_thing(keyword)
+    link = link_to(keyword.name, partners_path(keyword: keyword.name))
+
+    "<div class='keyword'>" +
+      "<h2>#{link} (#{keyword.partner_keywords.count})</h2>" +
+      "</div>"
+  end
+
+  def _render_event_thing(event_instance)
+    event = event_instance.event
+
+    link = link_to(event.name, event_path(event_instance))
+    "<div class='event'>" +
+      "<h2>#{link}</h2>" +
+      "<p>#{event.summary}</p>" +
+      "</div>"
+  end
+
+  def render_hot_feed(feed)
+    html_chunks = @hot_feed.map do |hot_thing|
+      case hot_thing
+      when Keyword then _render_keyword_thing(hot_thing)
+      when Partner then _render_partner_thing(hot_thing)
+      when EventInstance then _render_event_thing(hot_thing)
+      else
+        raise "Unknown hot_thing: #{hot_thing.class}"
+      end
+    end
+
+    html_chunks
+      # .map { |chunk| "#{chunk}</p>" }
+      .join("\n<br />\n")
+      .html_safe
+
+  end
+
+  def render_hot_keywords(keywords)
+    keywords
+      .map { |kw|
+        link = link_to(kw.name,
+                       partners_path(keyword: kw.name),
+                       class: 'keyword')
+        "#{link} (#{kw.partner_keywords.count})"
+      }
+      .join(' &bull; ')
+      .html_safe
+  end
+  # lordy this isn't very testable, lol
 end
